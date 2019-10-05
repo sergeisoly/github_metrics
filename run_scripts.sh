83c60f7a782d11abe634591354e3fc401cb82e75
sergeisoly <40268784+sergeisoly@users.noreply.github.com>
2019-10-06 00:36:59 +0300

    Delete run_scripts.sh

--- run_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-repos=('opencv/opencv' 'opencv/CVAT' 'opencv/open_model_zoo' 'opencv/openvino_training_extensions' 'opencv/dldt')
-
-for repo in in "${repos[@]}"; do
-    python github_metrics.py -repo "$repo" 
-done
